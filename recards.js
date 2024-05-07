0f306267f623296048509b3cba41621b74aa2079
Naval Rathore <94531756+navalrathore10@users.noreply.github.com>
2024-05-07 15:24:25 +0530

    Add files via upload
    This repository presents a dynamic card design suitable for potential inclusion on Royal Enfield's official website. With interactive color selection and detailed model information, users can explore various Royal Enfield motorcycles. The design offers a visually engaging experience, showcasing the brand's lineup effectively. Its clean and modern layout ensures accessibility across devices. Whether for enthusiasts or potential buyers, this card design enhances user interaction. With the potential for integration into Royal Enfield's official site, it aims to elevate the brand's online presence and provide a seamless browsing experience for visitors.

---
+++ recards.js
@@ -0,0 +1,187 @@
+// +++++++++++++++++++++++++++++++++++++++++++++++++ GT Color JS Code Starts
+
+function gtColor1(clickedDiv) {
+    event.preventDefault();
+    var targetDiv = document.getElementById('colorChange_gt');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/slipstream-blue/new/slipstream_blue_0000.png")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/slipstream-blue/new/slipstream_blue_0000.png")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/slipstream-blue/new/slipstream_blue_0000.png")';
+    }
+
+    var targetDiv = document.getElementById('changeBorder_gt');
+
+    if (targetDiv.style.border === '4px solid #256fef;' && targetDiv.style.outline === '2px solid #90c4ef;') {
+        targetDiv.style.border = '4px solid #256fef;';
+        targetDiv.style.outline = '2px solid #90c4ef;';
+    } else {
+        targetDiv.style.border = '4px solid #256fef;';
+        targetDiv.style.outline = '2px solid #90c4ef;';
+    }
+
+}
+
+function gtColor2(clickedDiv) {
+    event.preventDefault();
+    var targetDiv = document.getElementById('colorChange_gt');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/apex-grey/new/apex_grey_0000.png")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/apex-grey/new/apex_grey_0000.png")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/apex-grey/new/apex_grey_0000.png")';
+    }
+
+
+    var targetDiv = document.getElementById('changeBorder_gt');
+
+    if (targetDiv.style.border === '4px solid #cacaca;' && targetDiv.style.outline === '2px solid #ef8025;') {
+        targetDiv.style.border = '4px solid #cacaca;';
+        targetDiv.style.outline = '2px solid #ef8025;';
+    } else {
+        targetDiv.style.border = '4px solid #cacaca;';
+        targetDiv.style.outline = '2px solid #ef8025;';
+    }
+}
+
+function gtColor3(clickedDiv) {
+    event.preventDefault();
+    var targetDiv = document.getElementById('colorChange_gt');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/dux-dulux/new/dux_deulxe_0000.png")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/dux-dulux/new/dux_deulxe_0000.png")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/dux-dulux/new/dux_deulxe_0000.png")';
+    }
+}
+
+function gtColor4(clickedDiv) {
+    event.preventDefault();
+    var targetDiv = document.getElementById('colorChange_gt');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/rocker-red/new/rocker_red_0000.png")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/rocker-red/new/rocker_red_0000.png")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/rocker-red/new/rocker_red_0000.png")';
+    }
+}
+
+function gtColor5(clickedDiv) {
+    event.preventDefault();
+    var targetDiv = document.getElementById('colorChange_gt');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/mr-clean/new/mr_clean_0000.png")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/mr-clean/new/mr_clean_0000.png")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/continental-gt/new/colours/studio-shots/mr-clean/new/mr_clean_0000.png")';
+    }
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++ GT Color JS Code Ends
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++ Hunter Color JS Code Starts
+
+
+function hntrColor1(clickedDiv) {
+    event.preventDefault();
+    var targetDiv = document.getElementById('colorChange_hntr');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/rebel-black/rebel_black_000.webp")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/rebel-black/rebel_black_000.webp")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/rebel-black/rebel_black_000.webp")';
+    }
+}
+function hntrColor2(clickedDiv) {
+    event.preventDefault();
+
+    var targetDiv = document.getElementById('colorChange_hntr');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/rebel-blue/rebel_blue_000.webp")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/rebel-blue/rebel_blue_000.webp")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/rebel-blue/rebel_blue_000.webp")';
+    }
+}
+function hntrColor3(clickedDiv) {
+    event.preventDefault();
+
+    var targetDiv = document.getElementById('colorChange_hntr');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/rebel-red/rebel_red_000.webp")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/rebel-red/rebel_red_000.webp")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/rebel-red/rebel_red_000.webp")';
+    }
+}
+function hntrColor4(clickedDiv) {
+    event.preventDefault();
+
+    var targetDiv = document.getElementById('colorChange_hntr');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/dapper-grey/dapper_grey_000.webp")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/dapper-grey/dapper_grey_000.webp")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/dapper-grey/dapper_grey_000.webp")';
+    }
+}
+function hntrColor5(clickedDiv) {
+    event.preventDefault();
+
+    var targetDiv = document.getElementById('colorChange_hntr');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/factory-black/factory_black_000.webp")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/factory-black/factory_black_000.webp")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/factory-black/factory_black_000.webp")';
+    }
+}
+function hntrColor6(clickedDiv) {
+    event.preventDefault();
+
+    var targetDiv = document.getElementById('colorChange_hntr');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/stellar-red/stellar_red_000.webp")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/stellar-red/stellar_red_000.webp")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/stellar-red/stellar_red_000.webp")';
+    }
+}
+function hntrColor7(clickedDiv) {
+    event.preventDefault();
+
+    var targetDiv = document.getElementById('colorChange_hntr');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/halycon-green/halycon_green_000.webp")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/halycon-green/halycon_green_000.webp")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/halycon-green/halycon_green_000.webp")';
+    }
+}
+function hntrColor8(clickedDiv) {
+    event.preventDefault();
+
+    var targetDiv = document.getElementById('colorChange_hntr');
+
+    // Toggle background color of the target div
+    if (targetDiv.style.backgroundImage === 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/dapper-white/dapper_white_000.webp")') {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/dapper-white/dapper_white_000.webp")';
+    } else {
+        targetDiv.style.backgroundImage = 'url("https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/hunter-350/colors/dapper-white/dapper_white_000.webp")';
+    }
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++ Hunter Color JS Code Ends
